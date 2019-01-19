b77eead5ec0670310ddcaedcfa8bf3d491c12f80
PankajB1997 <pankajbhootra@gmail.com>
2019-01-19 21:24:45 +0800

    Add method to get full summary for a given job url

--- job_search/indeed.py
+++ job_search/indeed.py
@@ -5,11 +5,13 @@ import pandas as pd
 import time
 
 def extract_job_title_from_result(soup):
-  jobs = []
+  job_titles = []
+  job_links = []
   for div in soup.find_all(name="div", attrs={"class":"row"}):
     for a in div.find_all(name="a", attrs={"data-tn-element":"jobTitle"}):
-      jobs.append(a["title"])
-  return(jobs)
+      job_titles.append(a["title"])
+      job_links.append("https://www.indeed.com.sg" + a["href"])
+  return job_titles, job_links
 
 def extract_company_from_result(soup):
   companies = []
@@ -52,31 +54,44 @@ def extract_summary_from_result(soup):
     summaries.append(span.text.strip())
   return(summaries)
 
-def search_by_keyword(keyword, location, limit):
+def get_description_for_given_job_url(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+    eles = soup.findAll(attrs={"class": "jobsearch-JobComponent-description"})
+    for ele in eles:
+        return ele.text
+
+def search_indeed_jobs(keyword, location):
+  limit = 10
   # url = "https://www.indeed.com.sg/jobs?q=software+engineer&l=Singapore&start=10"
   # &as_phr=b&as_any=c&as_not=d&as_ttl=e&as_cmp=f&as_src=g&sort=&psf=advsrch
   url = "https://www.indeed.com.sg/jobs?as_and=" + keyword.lower().strip().replace(" ", "+") + "&l=" + location.lower().strip() + "&limit=" + str(limit) + "&jt=all&radius=10&fromage=7"
   page = requests.get(url)
   # print(url)
   soup = BeautifulSoup(page.text, "html.parser")
-  job_titles = extract_job_title_from_result(soup)
+  job_titles, job_links = extract_job_title_from_result(soup)
   companies = extract_company_from_result(soup)
   locations = extract_location_from_result(soup)
   salaries = extract_salary_from_result(soup)
-  summaries = extract_summary_from_result(soup)
+  # summaries = extract_summary_from_result(soup)
   jobs = []
   for i in range(len(job_titles)):
     job = {}
+    job['link'] = job_links[i]
     job['job_title'] = job_titles[i]
     job['company'] = companies[i]
     job['location'] = locations[i]
     # job['salary'] = salaries[i]
-    job['summary'] = summaries[i]
+    job['summary'] = get_description_for_given_job_url(job_links[i])
     jobs.append(job)
   if len(jobs) > limit:
       jobs = jobs[:limit]
   return jobs
 
-# jobs = search_by_keyword("data scientist", "Singapore", 10)
+# import time
+# start = time.time()
+jobs = search_indeed_jobs("data scientist", "Singapore")
+# end = time.time()
+# print(end-start)
 # for job in jobs:
-#     print(job['company'])
+#     print(job['summary'])
