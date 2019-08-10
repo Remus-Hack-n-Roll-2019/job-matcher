e488d41703c1953e09f6756c75b7151bd5e44557
Aadit Kamat <aadit.k12@gmail.com>
2019-08-10 08:36:20 +0800

    Handle the HTTP requests sequentially due to gevent incompatibility
    Signed-off-by: Aadit Kamat <aadit.k12@gmail.com>

--- job_search/parallelize.py
+++ job_search/parallelize.py
@@ -1,35 +1,8 @@
-import requests
-import bs4
-from bs4 import BeautifulSoup
-import pandas as pd
-import time
-from gevent.pool import Pool
-from gevent import monkey; monkey.patch_all() # patches stdlib
-import sys
-import logging
-from http.client import HTTPSConnection
-from timeit import default_timer as timer
-from job_search.indeed import *
-info = logging.getLogger().info
 
-def parallelize_requests(f, listOfRequests):
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
-    pool = Pool(20) # limit number of concurrent connections
-    start = timer()
-    listOfResponses = pool.map(f, listOfRequests)
-    info("%d hosts took us %f seconds", len(listOfRequests), timer() - start)
-    return listOfResponses
 
-# if __name__=="__main__":
-#     strings = [
-#         'Deep Learning', 'software engineer', 'test engineer', 'devops engineer', 'mechanical engineer', 'entrepreneur',
-#         'Deep Learning', 'software engineer', 'test engineer', 'devops engineer'
-#     ]
-#     listOfRequests = []
-#     for keyword in strings:
-#         listOfRequests.append({ "keyword": keyword, "location": "singapore" })
-#     listOfResponses = parallelize_requests(listOfRequests)
-#     responses = []
-#     for response in listOfResponses:
-#         responses.extend(response)
-#     print(len(responses))
+# TODO: Parallelize the requests
+def parallelize_requests(f, requests):
+    results = []
+    for request in requests:
+        results.append(f(request))
+    return results
